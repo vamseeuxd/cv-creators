58858a8bdce475c2ce59f67729c7520de4db64b6
vamseeuxd <vamsee.uxd@gmail.com>
2024-10-27 12:33:45 +0530

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"cv-creators","appId":"1:912095605819:web:394a19810dd2026d4c181e","storageBucket":"cv-creators.appspot.com","apiKey":"AIzaSyDXa5ynH7ukJWYRRSaYoKYeXgD9L7ZvCBw","authDomain":"cv-creators.firebaseapp.com","messagingSenderId":"912095605819","measurementId":"G-26LPS7LJ07"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
